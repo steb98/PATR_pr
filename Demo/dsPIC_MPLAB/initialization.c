999a71474cc7cae4413c0dbf2323a5ad69f48dfc
andreipaciurca <paciurca.andrei@outlook.com>
2021-05-10 14:29:49 +0300

    test

--- Demo/dsPIC_MPLAB/initialization.c
+++ Demo/dsPIC_MPLAB/initialization.c
@@ -29,33 +29,43 @@ void initPLL(void)
 
 //init AD Conversion
 void initAdc1(void){
-	AD1CON1bits.AD12B = 1; // conversie AD pe 12 biti
-	AD1CON1bits.FORM = 0; // rezultat conversie integer
-	AD1CON1bits.SSRC = 2; // timerul 3 starteaza conversia
-	AD1CON1bits.ASAM = 1; // incepe esantionarea noii valori imediat dupa terminarea
-	// unei conversii
-	AD1CON2bits.CSCNA = 1; // scaneaza intrarile pe CH0+ in timpul achizitiei A
-	AD1CON2bits.CHPS = 0; // converteste doar CH0
-	AD1CON2bits.SMPI = 0; // incrementeaza adresa DMA dupa terminarea fiecarei conversii
-	AD1CON3bits.ADRC = 0; // foloseste ceasul magistralei
-	AD1CON3bits.ADCS = 63; // Timpul necesar unei conversii este de 22.4 us
+	AD1CON1bits.AD12B = 1; 	// conversie AD pe 12 biti
+	AD1CON1bits.FORM = 0;  	// rezultat conversie integer
+	AD1CON1bits.SSRC = 2;  	// timerul 3 starteaza conversia
+	AD1CON1bits.ASAM = 1;  	// incepe esantionarea noii valori imediat dupa terminarea
+							   // unei conversii
+	AD1CON2bits.CSCNA = 1; 	// scaneaza intrarile pe CH0+ in timpul achizitiei A
+	AD1CON2bits.CHPS = 0; 	// converteste doar CH0
+	AD1CON2bits.SMPI = 0;  	// incrementeaza adresa DMA dupa terminarea fiecarei
+							   // conversii
+
+	AD1CON3bits.ADRC = 0;  	// foloseste ceasul magistralei
+	AD1CON3bits.ADCS = 63; 	// Timpul necesar unei conversii este de 19.2 us
+							   // Ceasul pentru conversia AD are formula Tad=Tcy*(adcs+1)
+							   // Tad=Tcy*(adcs+1)=(1/40)*64=1.6us
+
+// Se seteaza intrarile analogice
 	AD1CSSLbits.CSS5 = 1;  	// Selectam intrarea analogica AN5(RB3) pentru a fi scanata
-	// Scriem registrul de configurare al portului
-	// Se va folosi doar registrul low al portului de configurare deoarece dsPIC33fj128MC802
-	// nu are implementati mai mult de 6 pini pentru ADC
-	AD1PCFGL=0xFFFF; // Setam toti pinii portului ADC1 pe modul digital,
-	// si activeaza citirea la intrarea portului
-	AD1PCFGLbits.PCFG4 = 0;// Setam pinul AN4(RB2) pe intrare analogica,
-	// ADC verifica VOLTAGEele pe acel pin (achizitie AD)
-	IFS0bits.AD1IF = 0; // Reseteaza flag-ul intreruperii convertorului AD
-	IPC3bits.AD1IP = 6; // Seteaza prioritatea intreruperii convertorului AD
-	IEC0bits.AD1IE = 1; // Permite intreruperea convertorului AD
-	AD1CON1bits.ADON = 1; //Startare conversie
+
+// Scriem registrul de configurare al portului
+// Se va folosi doar registrul low al portului de configurare deoarece dsPIC33fj128MC802
+// nu are implementati mai mult de 6 pini pentru ADC
+
+	AD1PCFGL=0xFFFF;       	// Setam toti pinii portului ADC1 pe modul digital,
+							// si activeaza citirea la intrarea portului
+	_TRISB3 = 1;
+	AD1PCFGLbits.PCFG5 = 0;	// Setam pinul AN5(RB3) pe intrare analogica,
+							// ADC verifica voltajele pe acel pin (achizitie AD)
+	IFS0bits.AD1IF = 0;     // Reseteaza flag-ul intreruperii convertorului AD
+	IPC3bits.AD1IP = 6;     // Seteaza prioritatea intreruperii convertorului AD
+	IEC0bits.AD1IE = 1;     // Permite intreruperea convertorului AD
+
+	AD1CON1bits.ADON = 1;
 }
 
 void initTmr3(){
 	TMR3 = 0; //Resetam timer 3
-	T3CONbits.TCKPS = 3; //PRESCALER de 1:256
+	T3CONbits.TCKPS = 2; //PRESCALER
 	PR3 = 15625; // T = PR3/(FCY/PRESCALER); T = 0.1 s, FCY = 40 MHz; PRESCALER=256 => PR3=15625 
 	T3CONbits.TON = 1; // Start Timer 3
 }
